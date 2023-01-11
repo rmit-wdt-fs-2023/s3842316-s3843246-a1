82025c983d7630ca9e2a16a8dee1465ea3ca7668
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2023-01-12 03:30:13 +1100

    Changed Login Implementation to DB & Sperated Classes

--- MCBAProj/MCBABackend/Utilities/MiscUtils.cs
+++ MCBAProj/MCBABackend/Utilities/MiscUtils.cs
@@ -1,7 +1,23 @@
-﻿namespace MCBA.Utils;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MCBA.Utils;
 
 internal static class MiscUtils
 {
-	internal static char Deposit = 'D';
-	internal static object GetObjOrDbNull(this object value) => value ?? DBNull.Value; 
+    // Method Adapted from Rmit/Week3/WebServiceAndDatabaseExample
+    internal static DataTable GetDataTable(this SqlCommand command)
+    {
+        using var adapter = new SqlDataAdapter(command);
+
+        var table = new DataTable();
+        adapter.Fill(table);
+
+        return table;
+    }
+
+    // Method Adapted from Rmit/Week3/WebServiceAndDatabaseExample
+    internal static object GetObjOrDbNull(this object value) => value ?? DBNull.Value;
+
+    internal static char Deposit = 'D';
 }
